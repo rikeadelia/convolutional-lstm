a8e2dd977f3783bb0a200d6c4f1433316df4467d
Rike Adelia <adelia.rike@gmail.com>
2020-03-30 13:44:27 +0700

    make computation on a spesific cell

--- convlstm.py
+++ convlstm.py
@@ -5,11 +5,9 @@ from utils import ConvNorm
 #TODO: bikin multilayer
 #TODO: fixing input yang tidak batch first
 #TODO: mampu menangani PackedSequence
-class ConvLstm(torch.nn.Module):
-    def __init__(self, hidden_dim, num_layers=1, batch_first=True, w_init_gain='linear'):
-        super(ConvLstm, self).__init__()
-
-        # self.input_size = input_dim
+class ConvLstmCell(torch.nn.Module):
+    def __init__(self, hidden_dim, batch_first=True, w_init_gain='linear'):
+         # self.input_size = input_dim
         # self.batch_size = batch_size
         self.hidden_size = hidden_dim
         self.num_layers = num_layers
@@ -24,6 +22,63 @@ class ConvLstm(torch.nn.Module):
         self.b_f = torch.nn.Parameter(torch.randn(self.hidden_size, 1))
         self.b_c = torch.nn.Parameter(torch.randn(self.hidden_size, 1))
         self.b_o = torch.nn.Parameter(torch.randn(self.hidden_size, 1))
+
+    def convlstm_packed():
+        #pass
+
+    def convlstm_full(self, timestep, prev_hidden, prev_cell, input_size):
+        #expand num of weights to batch size
+        W_i = self.W_i.expand(self.hidden_size, input_size)
+        W_f = self.W_f.expand(self.hidden_size, input_size)
+        W_o = self.W_o.expand(self.hidden_size, input_size)
+
+        #expand num of biases to batch size
+        b_i = self.b_i.expand(self.hidden_size, input_size)
+        b_f = self.b_f.expand(self.hidden_size, input_size)
+        b_c = self.b_c.expand(self.hidden_size, input_size)
+        b_o = self.b_o.expand(self.hidden_size, input_size)
+
+        x_i = ConvNorm(self.hidden_size, self.hidden_size)(timestep.unsqueeze(0)).squeeze(0)
+        x_f = ConvNorm(self.hidden_size, self.hidden_size)(timestep.unsqueeze(0)).squeeze(0)
+        x_c = ConvNorm(self.hidden_size, self.hidden_size)(timestep.unsqueeze(0)).squeeze(0)
+        x_o = ConvNorm(self.hidden_size, self.hidden_size)(timestep.unsqueeze(0)).squeeze(0)
+
+        h_i = ConvNorm(self.hidden_size, self.hidden_size)(htm1.unsqueeze(0)).squeeze(0) #convolution for hidden of ingate
+        h_f = ConvNorm(self.hidden_size, self.hidden_size)(htm1.unsqueeze(0)).squeeze(0) #convolution for hidden of forgetgate
+        h_c = ConvNorm(self.hidden_size, self.hidden_size)(htm1.unsqueeze(0)).squeeze(0) #convolution for hidden of cellgate
+        h_o = ConvNorm(self.hidden_size, self.hidden_size)(htm1.unsqueeze(0)).squeeze(0) #convolution for hidden of outgate
+
+        ingate = torch.sigmoid(x_i + h_i + torch.mul(W_i, ctm1) + b_i)
+        forgetgate = torch.sigmoid(x_f + h_f + torch.mul(W_f, ctm1) + b_f)
+        ct = torch.mul(forgetgate, ctm1) + ingate + torch.tanh(x_c + h_c + b_c)
+        outgate = torch.sigmoid(x_o + h_o + torch.mul(W_o, ct) + b_o)
+        ht = torch.mul(outgate, torch.tanh(ct))
+
+        return ht, ct
+
+    def forward(self, x, prev_states, input_size):
+        if is_packed:
+            convlstm_packed()
+        else:
+            result = convlstm_full(x, prev_states[0], prev_states[1], input_size)
+
+        return result
+
+class ConvLstm(torch.nn.Module):
+    def __init__(self, hidden_dim, num_layers=1, batch_first=True, w_init_gain='linear'):
+        super(ConvLstm, self).__init__()
+
+        # self.input_size = input_dim
+        # self.batch_size = batch_size
+        self.hidden_size = hidden_dim
+        self.num_layers = num_layers
+        self.batch_first = batch_first
+
+        layers = []
+        for i in range(num_layers):
+            layers.append(ConvLstmCell(self.hidden_dim, self.batch_first))
+        
+        self.layers = nn.ModuleList(layers)
 
     def conv_lstm_cell(self, input_, initial_hidden, initial_cell):
         input_size = input_.size(0)
@@ -96,22 +151,35 @@ class ConvLstm(torch.nn.Module):
         #                         max_batch_size, self.hidden_size,
         #                         dtype=x.dtype, device=x.device)
         #     hx = (zeros, zeros)
-        # else:
+        # else:hiddens.append(ht.unsqueeze(0))
         #     # Each batch of the hidden state should match the input sequence that
         #     # the user believes he/she is passing in.
         #     hx = self.permute_hidden(hx, sorted_indices)
 
         # self.check_forward_args(x, hx, batch_sizes)
-        # if batch_sizes is None:
-        #     # result = _VF.lstm(x, hx, self._flat_weights, self.bias, self.num_layers,
-        #                     #   self.dropout, self.training, self.bidirectional, self.batch_first)
+        if batch_sizes is None:
+            batch_size = x.size(0)
+            current_layer_input = x.permute(1, 2, 0) # (seq_len, hidden_size, batch)
+
+            for layers in self.layers:
+                htm1 = initials[0] #ht-1
+                ctm1 = initials[1] #ct-1
+                hiddens = []
+                for timestep in current_layer_input:
+                    ht, ct = layers(timestep, htm1, ctm1, batch_size)
+                    hiddens.append(ht.unsqueeze(0)) # (1, hidden_size, batch_size)
+
+                    htm1, ctm1 = ht, ct
+                
+                current_layer_input = torch.cat(hiddens, dim=0)
+
         #     for i in range(x.size(0)):
         #         hx, cx = conv_lstm_cell(x[i], hx, cx, w_ih, w_hh, b_ih, b_hh)
         # else:
-        result = self.conv_lstm_cell(x, initials[0], initials[1])
+        # result = self.conv_lstm_cell(x, initials[0], initials[1])
 
-        output = result[0]
-        hidden = result[1:]
+        # output = result[0]
+        # hidden = result[1:]
         # xxx: isinstance check needs to be in conditional for TorchScript to compile
         # if isinstance(orig_input, PackedSequence):
         #     output_packed = PackedSequence(output, batch_sizes, sorted_indices, unsorted_indices)
@@ -119,14 +187,16 @@ class ConvLstm(torch.nn.Module):
         # else:
         #     return output, self.permute_hidden(hidden, unsorted_indices)
         if self.batch_first:
-            output = output.permute(2, 0, 1) #batch, seq_len, hidden_size
-        return output, (result[0], result[1])
+            output = current_layer_input.permute(2, 0, 1) # (batch, seq_len, hidden_size)
+        else:
+            output = current_layer_input
+        return output, (ht, ct)
 
 if __name__ == '__main__':
     convlstm = ConvLstm(256)
     x = torch.ones([12, 10, 256])
-    initial_h = torch.zeros([12, 256])
-    initial_c = torch.zeros([12, 256])
+    initial_h = torch.zeros([256, 12])
+    initial_c = torch.zeros([256, 12])
     y, _ = convlstm(x, [initial_h, initial_c])
     print(y.size())
     test_data = torch.ones([1, 10, 256])
